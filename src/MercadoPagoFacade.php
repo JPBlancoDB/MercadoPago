70a17bcc029e6fd3421c298a9692905a7a9d53d5
JP Blanco <jpblancodb@gmail.com>
2016-06-15 12:02:06 -0300

    Added missing use MP

--- src/MercadoPagoFacade.php
+++ src/MercadoPagoFacade.php
@@ -1,10 +1,10 @@
 <?php namespace JPBlancoDB\MercadoPago;
 
-
 use Illuminate\Support\Facades\Facade;
 
 class MercadoPago extends Facade
 {
+
     protected static function getFacadeAccessor()
     {
         return 'mercadopago';
--- src/MercadoPagoProvider.php
+++ src/MercadoPagoProvider.php
@@ -1,23 +1,21 @@
-
 <?php namespace JPBlancoDB\MercadoPago;
 
 use Illuminate\Support\ServiceProvider;
+use MP;
 
-class MercadoPagoProvider extends ServiceProvider 
+class MercadoPagoProvider extends ServiceProvider
 {
-	
-	public function boot()
-	{
-		$this->publishes([__DIR__.'/config/mercadopago.php' => config_path('mercadopago.php')]);
-	}
-	
-	public function register()
-	{
-		$this->app->bind('mercadopago', function(){
-			return new MP(
-				config('mercadopago.mp_client'), 
-				config('mercadopago.mp_secret') 
-			);
-		});
-	}
+
+    public function boot()
+    {
+        $this->publishes([ __DIR__ . '/config/mercadopago.php' => config_path('mercadopago.php') ]);
+    }
+
+
+    public function register()
+    {
+        $this->app->bind('mercadopago', function () {
+            return new MP(config('mercadopago.mp_client'), config('mercadopago.mp_secret'));
+        });
+    }
 }
